ba5daeb9dfed4e625922fa6487129e4eedc49b80
adamtrommlitz <adamtrommlitz3@gmail.com>
2024-03-13 20:49:59 -0600

    the best commits

---
+++ BowlingAPI/Controllers/BowlingController.cs
@@ -0,0 +1,23 @@
+﻿using BowlingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace BowlingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BowlerController : ControllerBase 
+    {
+        private IBowlingRepository _bowlingRepository;
+        public BowlerController(IBowlingRepository temp)
+        {
+            _bowlingRepository = temp;
+        }
+
+        [HttpGet("marlins-sharks")]
+        public IActionResult GetBowlersForMarlinsAndSharks()
+        {
+            var bowlers = _bowlingRepository.GetBowlersForMarlinsAndSharks();
+            return Ok(bowlers);
+        }
+    }
+}
---
+++ BowlingAPI/Models/EFBowlingRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BowlingAPI.Models
+{
+    public class EFBowlingRepository : IBowlingRepository
+    {
+        private BowlingLeagueContext _bowlingContext; //create instance of food context file
+        public EFBowlingRepository(BowlingLeagueContext temp)
+        {
+            _bowlingContext = temp;
+        }
+        //public IEnumerable<Bowler> Bowlers => _bowlingContext.Bowlers;
+        public IEnumerable<Bowler> GetBowlersForMarlinsAndSharks()
+        {
+            return _bowlingContext.Bowlers
+                .Include(b => b.Team) // Include the Team navigation property
+                .Where(b => b.Team != null && (b.Team.TeamName == "Marlins" || b.Team.TeamName == "Sharks"))
+                .ToList(); // Execute query and return as list
+        }
+    }
+}
